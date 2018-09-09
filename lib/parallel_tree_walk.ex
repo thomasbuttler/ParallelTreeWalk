86abaa310cdca9e9af760c0784341efa858713f4
thomasbuttler <43081771+thomasbuttler@users.noreply.github.com>
2018-09-09 02:26:57 -0400

    In proc_dir/1, wait for poolboy processes to finish Fix bug that would have skipped device files Allow process_entry lambda to prune a directory Improve README Add .leo file

--- lib/parallel_tree_walk.ex
+++ lib/parallel_tree_walk.ex
@@ -1,4 +1,4 @@
-# thank you to https://githubcom/thestonefox/elixir_poolboy_example
+# thank you to https://github.com/thestonefox/elixir_poolboy_example
 defmodule ParallelTreeWalk do
   use Application
 
@@ -10,8 +10,8 @@ defmodule ParallelTreeWalk do
     poolboy_config = [
       {:name, {:local, pool_name()}},
       {:worker_module, ParallelTreeWalk.Worker},
-      {:size, 2},
-      {:max_overflow, 1}
+      {:size, :erlang.system_info(:thread_pool_size)},
+      {:max_overflow, 0}
     ]
 
     children = [
@@ -26,17 +26,20 @@ defmodule ParallelTreeWalk do
     Supervisor.start_link(children, options)
   end
 
-
   # major device numbers are unique to the file system, and minor numbers are always zero,
   # *except* when the file is a device (block or character).  Practically, it means we can
   # use a difference in the major device number of directories to identify a mount point.
   def procdir(path_name) do # /1 example
     {:ok, %File.Stat{major_device: major}} = File.lstat(path_name)
-    proc_entry = fn(name, _stat) -> IO.puts("#{name}") end
+    proc_entry = fn(name, _stat) ->
+      IO.puts("#{name}")
+      :true
+    end
     filter_entry = fn(_name) -> :true end      # accept all, for this demo
     procdir(path_name, major, proc_entry, filter_entry)
+    wait_for_empty_poolboy()
   end
-  
+
   def procdir(path_name, major, proc_entry, filter_entry) do # /4 version
     case :poolboy.checkout(pool_name(), :false) do
       # :false above means do not block
@@ -51,6 +54,18 @@ defmodule ParallelTreeWalk do
             :poolboy.checkin(pool_name(), pid)
           end
         end)
+    end
+  end
+
+  def main(_) do
+    procdir(".")
+  end
+
+  defp wait_for_empty_poolboy() do
+    case :poolboy.status(pool_name()) do
+    {_, _, _, 0} -> :ok
+    _            -> :timer.sleep(10)
+                    wait_for_empty_poolboy()
     end
   end
 
--- lib/parallel_tree_walk/proc_dir.ex
+++ lib/parallel_tree_walk/proc_dir.ex
@@ -7,31 +7,35 @@ defmodule ParallelTreeWalk.ProcDir do
         case file_stat do
           # todo: develop design such that "major" can indicate we *do* want to cross file system boundaries
           %File.Stat{major_device: ^major} -> procdir(data, file_stat)
-          _                                -> :will_not_cross_mount_points
+          %File.Stat{type: :directory}     -> :will_not_cross_mount_points
+          _                                -> procdir(data, file_stat) # still process device with differing major
         end
       result           ->
         Logger.warn("File.lstat of #{path_name} repeatedly failed: #{inspect(result)}")
     end    
   end
-  
+
   def procdir({path_name, major, proc_entry, proc_filter}, file_stat) do
-    proc_entry.(path_name, file_stat)
-    case file_stat do
-      %File.Stat{type: :directory} ->
-        case retry(fn() -> :file.list_dir_all(path_name) end, 0, 10) do
-          # note recursion to top level of module, where pool checkin/checkout occur
-          {:ok, entries} ->
-            for entry <- entries, proc_filter.(entry), do:
-              ParallelTreeWalk.procdir(Path.join(path_name,entry), major, proc_entry, proc_filter)
-          result         ->
-            Logger.warn(":file.list_dir_all of #{path_name} repeatedly failed: #{inspect(result)}")
-        end
-      _ -> :finished_with_non_directory
+    case proc_entry.(path_name, file_stat) do
+      :false -> :pruned
+      _      ->
+        case file_stat do
+        %File.Stat{type: :directory} ->
+          case retry(fn() -> :file.list_dir_all(path_name) end, 0, 10) do
+            # note recursion to top level of module, where pool checkin/checkout occur
+            {:ok, entries} ->
+              for entry <- entries, proc_filter.(entry), do:
+                ParallelTreeWalk.procdir(Path.join(path_name,entry), major, proc_entry, proc_filter)
+            result         ->
+              Logger.warn(":file.list_dir_all of #{path_name} repeatedly failed: #{inspect(result)}")
+          end
+        _ -> :finished_with_non_directory
+      end
     end
   end
 
   use Bitwise # for left shift operator <<<
-  
+
   defp retry(to_retry, count, max) when count < max do
     result = to_retry.()
     case result do
@@ -40,8 +44,10 @@ defmodule ParallelTreeWalk.ProcDir do
                       retry(to_retry, count+1, max)
     end
   end
+
   defp retry(to_retry, _, _) do
     # one last time
     to_retry.()
   end
+
 end
