11b2dba8cb2213c1697aa0de3b210e91f8bc633e
thomasbuttler <43081771+thomasbuttler@users.noreply.github.com>
2018-09-08 20:10:14 -0400

    Add files via upload

---
+++ mix.exs
@@ -0,0 +1,38 @@
+# cargo culted from https://github.com/thestonefox/elixir_poolboy_example/blob/master/mix.exs
+defmodule ParallelTreeWalk.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :parallel_tree_walk,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [
+      mod: {ParallelTreeWalk, []},
+      applications: [:logger, :poolboy]
+    ]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    [
+      {:poolboy, "~> 1.5"}
+    ]
+  end
+end
