71a8ec2e6575b75fa54bbe0295f9271801e70cda
thomasbuttler <43081771+thomasbuttler@users.noreply.github.com>
2018-11-25 20:54:34 -0500

    First substantial test.

--- test/ParallelTreeWalk_test.exs
+++ test/ParallelTreeWalk_test.exs
@@ -1,8 +1,13 @@
 defmodule ParallelTreeWalkTest do
   use ExUnit.Case
 
-  test "stub for first travis ci" do
-    assert 1 == 1
+  test "basic traversal works" do
+    test_files = ["test/dir/a.txt", "test/dir/b.txt", "test/dir/badlink", "test/dir/c/d/f.txt", "test/dir/goodlink"]
+    {:ok, walker} = DirWalker.start_link("test/dir")
+    files = DirWalker.next(walker, 99)
+    assert length(files) == 5
+    assert Enum.sort(files) == Enum.sort(test_files)
   end
+
 
 end
